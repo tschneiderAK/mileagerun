775011b2fa966eb308e6a31c891abeb1d7e2a75b
Tom Schneider <schneider.thomas.g@gmail.com>
2022-01-23 17:16:26 -0900

    Fixed issue causing 500 error: only certain origins work in dev environment, origin is now PAR. Dump to JSON implemented.

--- src/amadeus_calls.py
+++ src/amadeus_calls.py
@@ -1,34 +1,33 @@
 """Uses the Amadeus APIs to extract data from JSON objects."""
 
 
+from http import client
 import json
 from csv import DictReader
 from pathlib import Path
 from posixpath import relpath
-from amadeus import Client
+from amadeus import Client, ResponseError
 
 
 def main():
-    """Retrieves authorization tokens, initiates amadeus client, and makes a call to find flight destinations, which are saved to file.
+    """Initiates amadeus client, and makes a call to find flight destinations, which are saved to file.
     
     Parameters:
     Origin (str): IATA airport or 3 letter city code. Currently hardcoded to 'JFK'
     
     """
+    amadeus = initialize_client()
+    response = flight_destinations(amadeus, 'PAR')
+    results = relpath("../data/result.json")
+    with open(results, 'w') as f:
+        json.dump(response.data, f)
+
+
+def initialize_client():
     api_dict = retrieve_api_keys()
     api_key= api_dict['api_key']
     api_secret = api_dict['api_secret']
-    print(api_key)
-    print(api_secret)
-
-    amadeus = Client(client_id=api_key, client_secret=api_secret)
-    response = flight_destinations(amadeus, 'JFK')
-
-    results = relpath("../data/result.json")
-    with open(results, 'rw') as f:
-        json.dump(response, f)
-
-
+    return Client(client_id=api_key, client_secret=api_secret)
 
 def retrieve_api_keys():
     """Pulls API keys from a csv.
@@ -50,13 +49,12 @@ def flight_destinations(amadeus: Client, origin: str):
     
     Paramters:
     amadeus:    The amadeus Client object initialized using API keys.
-    origin:     3 letter city or IATA code. Defaults to 'JFK'. 
+    origin:     3 letter city or IATA code. 
     """
     response = amadeus.shopping.flight_destinations.get(origin=origin)
     print(response.status_code)
     print(response.result)
     print(response.data)
-    print(response.body)
     print(response.body)
 
     return response
--- src/flights.py
+++ src/flights.py
@@ -1,12 +1,12 @@
 """Finds flights from a specified origin and list of destinations over a date range.
 
-args:
+Parameters:
     orgin (str):            3 letter IATA airport code for origin.
     destinations (.txt):    .txt file containing a destination airport code on each line.
     start (str):            Date for start of search range, formatted yyyy-mm-dd.
     end (str):              Date for end of search range, formatter yyyy-mm-dd.
     
-returns:
+Returns:
     itineraries (list):     List of itinerary objects."""
 
 
