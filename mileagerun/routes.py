38d6fa43929262acd0ce768c42f046932b9a6530
Tom Schneider <schneider.thomas.g@gmail.com>
2022-03-29 22:59:03 -0800

    Moved routes, models, to separate files. Moved templates into package.

---
+++ mileagerun/routes.py
@@ -0,0 +1,70 @@
+
+from flask import redirect, render_template, request, url_for, session, flash
+from mileagerun.models import User, earning
+from mileagerun import app, db
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html', values=User.query.all())
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        session.permanent = True
+        user = request.form['nm']
+        session['user'] = user
+        found_user =User.query.filter_by(name=user).first()
+        if found_user:
+            session['email'] = found_user.email
+        else:
+            usr = User(name=user, email=None)
+            db.session.add(usr)
+            db.session.commit()
+
+        flash(f"Successfully logged in as {user}.")
+        return redirect(url_for('userhome'))
+    else:
+        if 'user' in session:
+            user = session['user']
+            flash(f"Logged in as {user}.")
+            return redirect(url_for('userhome'))
+        return render_template('login.html')
+
+@app.route('/userhome', methods=['POST', 'GET']) 
+def userhome():
+    email = None
+    if 'user' in session:
+        user = session['user']
+
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+            found_user = User.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash('Email saved')
+        else:
+            if 'email' in session:
+                email = session['email']
+
+        return render_template('userhome.html', email=email, user=user)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout/')
+def logout():
+    if 'user' in session:
+        user = session['user']
+        flash(f"You have been logged out, {user}.", "info")
+    session.pop('user', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+
+@app.route('/earnings')
+def earnings():
+    values = db.session.query(earning).all()
+    return render_template('earnings.html', values=values)
