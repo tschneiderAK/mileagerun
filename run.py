89649c165bb2dde40de204d2111aac40dd9ef3fb
Tom Schneider <schneider.thomas.g@gmail.com>
2022-03-31 21:41:28 -0800

    Added call to env.bat to initialize environment variables on initial startup of venv.

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from mileagerun import db, app
+import subprocess, os
+
+if not os.environ['FLASK_SECRET_KEY']:
+    subprocess.call([r'C:\\Code\\flight-finder\\venv\\.env.bat'])
 
 if __name__ == '__main__':
     db.create_all()
