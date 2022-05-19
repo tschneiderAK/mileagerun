76d9b0883b08781b9b2e5fa12c6b9c7810748d35
Tom Schneider <schneider.thomas.g@gmail.com>
2022-05-18 20:39:18 -0800

    Changed getPartners into two functions.

--- mileagerun/static/scripts.js
+++ mileagerun/static/scripts.js
@@ -1,16 +1,35 @@
-async function getPartners() {
+async function getFlownToCredited() {
     let flown = document.getElementById('flown-airline-select');
     let credit = document.getElementById('credit-airline-select');
     
-    let response = await fetch('/partners/' + flown.value);
-    let partnerJSON = await response.json();
+    let response = await fetch('/flown-to-credited/' + flown.value);
+    let flownToCreditJSON = await response.json();
     
-    console.table(partnerJSON)
+    console.table(flownToCreditJSON)
     
     options = '';
-    for (let result of partnerJSON['credit airlines']) {
+    for (let result of flownToCreditJSON['credit airlines']) {
         options += '<option value="' + result + '">' + result + '</option>';
 
     credit.innerHTML = options;
     }
 }
+
+async function getCreditedToFlown() {
+    let flown = document.getElementById('flown-airline-select');
+    let credit = document.getElementById('credit-airline-select');
+    
+    let response = await fetch('/credited-to-flown/' + credit.value);
+    let creditToFlownJSON = await response.json();
+    
+    console.table(creditToFlownJSON)
+    
+    options = '';
+    for (let result of creditToFlownJSON['operating airlines']) {
+        options += '<option value="' + result + '">' + result + '</option>';
+
+    flown.innerHTML = options;
+    }
+}
+
+
