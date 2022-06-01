33f1fb78b14ab6ec613664ddaeb17e1b19787cbe
Tom Schneider <schneider.thomas.g@gmail.com>
2022-06-01 09:58:23 -0800

    Added updateFlown function. Added support for origin select field to use bootstrap-autocomplete

--- mileagerun/static/scripts.js
+++ mileagerun/static/scripts.js
@@ -1,3 +1,8 @@
+async function updateFlown() {
+    getFlownToCredited();
+    getFareCodes();
+}
+
 async function getFlownToCredited() {
     let flown = document.getElementById('flown-airline-select');
     let credit = document.getElementById('credit-airline-select');
@@ -32,6 +37,38 @@ async function getCreditedToFlown() {
     }
 }
 
+async function getFareCodes() {
+    let flown = document.getElementById('flown-airline-select');
+
+    if (flown.value) {
+        let fareCode = document.getElementById('fare-code-select');
+        let response = await fetch('/fare-codes/' + flown.value);
+        let fareCodesJSON = await response.json();
+    
+        console.table(fareCodesJSON)
+        
+        options = '';
+        for (let result of fareCodesJSON['codes']) {
+            options += '<option value="' + result + '">' + result + '</option>';
+    
+        fareCode.innerHTML = options;
+        }
+    }
+}
+
+// WTForms does not support passing the data-url property to form fields.
+// This function will add this property once loaded.
+// function addDataURL() {}
+//     let url = 'aiports.json';
+//     let field = document.getElementById('origin-select')
+//     field.
+
+
   $(document).ready(function(){
-    $('.combobox').combobox();
+
+    $('#origin-select').autoComplete({
+        resolverSettings: {
+            url: 'airports.json'
+        }
+    });
   });
