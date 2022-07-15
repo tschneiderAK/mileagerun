547a0d901274e09f3df316db41e2a9dc26014313
Tom Schneider <schneider.thomas.g@gmail.com>
2022-07-15 13:48:06 -0800

    Changed to accept UA values

--- data/earnings_parse.py
+++ data/earnings_parse.py
@@ -1,8 +1,7 @@
 import csv
 import os.path
-from connect_to_db import connect_to_db
 
-path = os.path.abspath("C:/Code/flight-finder/airline-data/earnings_rates.csv")
+path = os.path.abspath("C:/Code/mileagerun/data/ua_earnings_unprocessed.csv")
 headers = []
 new_rows = []
 with open(path, "r") as f:
@@ -12,15 +11,10 @@ with open(path, "r") as f:
         codes = [x.strip() for x in row['fare_code'].split(",")]
         for code in codes:
             temp = [row[value] for value in row.keys()]
-            temp[3] = code
+            temp[2] = code + temp[5]
             new_rows.append(temp)
 
-with open(os.path.abspath("C:/Code/flight-finder/airline-data/new_earnings.csv"), "w", newline='') as f:
+with open(os.path.abspath("C:/Code/mileagerun/data/ua_earnings.csv"), "w", newline='') as f:
     write = csv.writer(f)
     write.writerow(headers)
     write.writerows(new_rows)
-
-cnx = connect_to_db('c:/code/flight-finder/config/db_config.csv')
-cursor = cnx.cursor()
-sql = "INSERT INTO earning_rates VALUES "
-print(new_rows)
