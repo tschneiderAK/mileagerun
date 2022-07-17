af4214c99169360ff312b1089a4db894dd64bda1
Tom Schneider <schneider.thomas.g@gmail.com>
2022-07-16 21:28:55 -0800

    Removed dead code for .env variables

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-import subprocess, os
-
-# if 'FLASK_SECRET_KEY' not in os.environ:
-#     subprocess.call([r'C:\\Code\\flight-finder\\venv\\.env.bat'])
-
 from mileagerun import db, app
 
 if __name__ == '__main__':
