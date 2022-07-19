53039688c2d186edac3da1c579d3524c30273413
Tom Schneider <tgschnei@mtu.edu>
2022-07-18 22:18:27 -0800

    Removed debug statements from __init__. Altered docker-compose to correctly name images.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,13 +3,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
-print(__name__)
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] =os.environ['DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(minutes=1440)
-print(app.secret_key, app.config)
 
 db =SQLAlchemy(app)
 
